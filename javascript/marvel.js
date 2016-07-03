2d5014deed5ba01ad5e961ab2d839eda81f83c43
Elsa Josephs <Elsa Josephs>
2016-07-03 13:31:11 -0400

    3 colums to youtube and its css

--- javascript/marvel.js
+++ javascript/marvel.js
@@ -43,14 +43,6 @@ carousel();
 					return false;
 				});
 
-
-// $('#super').on('submit', function(){
-// 	getMarvelResponse();
-// 	return false;
-// })
-
-
-
 var PRIV_KEY = "df4fb8031dbc7cf2f465816737e0ea13379128b0"
 var PUBLIC_KEY = "a9a21fdf8a29de1099f5d2548b48a5d7";
 
@@ -115,7 +107,7 @@ function getYouTube() {
 	
 	var characterName  = $('#superHero').val();
 	var youtubeApi = "AIzaSyBMbfmuvlnHD76RoFoCgRrzoTR8yLU_QS8";
-	var youtubeUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ characterName +"&key=" + youtubeApi;
+	var youtubeUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ characterName + "originstory&key=" + youtubeApi;
 	console.log(youtubeUrl);
 	
 $.ajax({
@@ -123,18 +115,50 @@ $.ajax({
   		method: 'GET'
     })
     .done(function(data) {
-      // sort of a long dump you will need to sort throughurl
     	console.log(data);
 
+    	// for (var i = 0; i <= data.items.lenght; i++){
+    	// 	var videoId = data.items[i].id.videoId
+    	// 	var videoTitle = data.items[i].snippet.title
+    	// 	console.log(videoId)
+    	// 	console.log(videoTitle)
+    	// 	var videoFrame = "<iframe width='320' height='193' src='http://www.youtube.com/embed/"+videoId+"' frameborder='0' type='text/html'></iframe>"
+    	// 	var final="<div id='title'>"+videoTitle+"</div><div>"+videoFrame+"</div>"
 
-    	var videoId = data.items[0].id.videoId
-    	var videoTitle = data.items[0].snippet.title
-    	console.log(videoId)
-    	console.log(videoTitle)
-    	var videoFrame = "<iframe width='640' height='385' src='http://www.youtube.com/embed/"+videoId+"' frameborder='0' type='text/html'></iframe>"
-    	var final="<div id='title'>"+videoTitle+"</div><div>"+videoFrame+"</div>";
+    	// 	$('.contentVideos').html(final)
+    	// }
+    	var videoId1 = data.items[0].id.videoId
+    	var videoTitle1 = data.items[0].snippet.title
+    	console.log(videoId1)
+    	console.log(videoTitle1)
+    	var videoFrame = "<iframe width='320' height='193' src='http://www.youtube.com/embed/"+videoId1+"' frameborder='0' type='text/html'></iframe>"
+    	var final="<div id='title'>"+videoTitle1+"</div><div class='frame'>"+videoFrame+"</div>";
     	// videoTitle.append(videoFrame);
     	$('.contentVideos').html(final);
+
+    	var videoId2 = data.items[1].id.videoId
+    	var videoTitle2 = data.items[1].snippet.title
+    	console.log(videoId2)
+    	console.log(videoTitle2)
+    	var videoFrame = "<iframe width='320' height='193' src='http://www.youtube.com/embed/"+videoId2+"' frameborder='0' type='text/html'></iframe>"
+    	var final="<div id='title'>"+videoTitle2+"</div><div class='frame'>"+videoFrame+"</div>";
+    $('.contentVideos2').html(final);
+
+    var videoId3 = data.items[2].id.videoId
+    	var videoTitle3 = data.items[2].snippet.title
+    	console.log(videoId3)
+    	console.log(videoTitle3)
+    	var videoFrame = "<iframe width='320' height='193' src='http://www.youtube.com/embed/"+videoId3+"' frameborder='0' type='text/html'></iframe>"
+    	var final="<div id='title'>"+videoTitle3+"</div><div class='frame'>"+videoFrame+"</div>";
+    $('.contentVideos3').html(final);
+
+     var videoId4 = data.items[3].id.videoId
+    	var videoTitle4 = data.items[3].snippet.title
+    	console.log(videoId4)
+    	console.log(videoTitle4)
+    	var videoFrame = "<iframe width='320' height='193' src='http://www.youtube.com/embed/"+videoId4+"' frameborder='0' type='text/html'></iframe>"
+    	var final="<div id='title'>"+videoTitle4+"</div><div class='frame'>"+videoFrame+"</div>";
+    $('.contentVideos4').html(final);
     })
     // .fail(function(err){
     //   // the error codes are listed on the dev site
